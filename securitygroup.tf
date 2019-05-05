21441121196b95be0d2bd1925ef6a17af266f8d3
James Tsang <cgjames@hotmail.com>
2019-05-05 00:01:32 -0400

    small change on security group to allow engress

--- securitygroup.tf
+++ securitygroup.tf
@@ -8,6 +8,12 @@ resource "aws_security_group" "allowrdp" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     tags {
         Name = "allow_rdp"
     }
